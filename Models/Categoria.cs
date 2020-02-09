664b73e70c6ce7b2403a456750ecc682d2fd1829
dpadillaf <dpadillafonseca@gmail.com>
2020-02-09 08:57:42 -0500

    primer commit

---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebCF.Models
+{
+	public class Categoria
+	{
+		public int idcategoria { get; set; }
+		[Required(ErrorMessage = "Ingrese un nombre para la categororía")]
+		[StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe tener más de 50 caracteres, ni menos de 3 carteres.")]
+		[Display(Name = "Nombre")]
+		public string nombre { get; set; }
+		[StringLength(255, ErrorMessage = "La descripción no debe tener más de 255 caracteres")]
+		[Display(Name = "Descripción")]
+		public string descripcion { get; set; }
+		[Display(Name = "Estado")]
+		public bool? estado { get; set; }
+		public virtual ICollection<Producto> productos { get; set; }
+	}
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWebCF.Models
+{
+	public class Producto
+	{
+		public int idproducto { get; set; }
+		[Required(ErrorMessage = "Ingrese una categoría")]
+		public int idcategoria { get; set; }
+		[StringLength(64, ErrorMessage = "El código no debe tener más de 64 caracteres")]
+		[Display(Name = "Código")]
+		public string codigo { get; set; }
+		[Required(ErrorMessage = "Ingrese un nombre para el producto")]
+		[StringLength(100, ErrorMessage = "El nombre no debe tener más de 100 caracteres")]
+		[Display(Name = "Nombre")]
+		public string nombre { get; set; }
+		[Required(ErrorMessage = "Ingrese un precio de venta")]
+		[Display(Name = "Precio de venta")]
+		public Decimal precio_venta { get; set; }
+		[Required(ErrorMessage = "Ingrese el stock del producto")]
+		[Display(Name = "Stock")]
+		public int stock { get; set; }
+		[StringLength(255, ErrorMessage = "La descripción no debe tener más de 255 caracteres")]
+		[Display(Name = "Descripción")]
+		public string descripcion { get; set; }
+		[Display(Name = "Estado")]
+		public bool? estado { get; set; }
+		[Display(Name = "Categoría")]
+		public virtual  Categoria categoria { get; set; }
+	}
+}
